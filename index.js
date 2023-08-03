33537b95bcd8f9416517c80b2679baee4199fa5e
Lucas Navarro <navarrorlucas@gmail.com>
2023-08-03 18:43:11 +0200

    lab done

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const allRecipes = require("./data.json")
+
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipe-app';
+
+
 
 // Connection to the database "recipe-app"
 mongoose
@@ -16,7 +20,57 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log("Creando data")
+   // Run your code here, after you have insured that the connection was made
+return Recipe.create({
+  "title": "Sevillan Glazed Andalú Thighs",
+  "level": "Amateur Chef",
+  "ingredients": [
+    "1/2 cup rice vinegar",
+    "5 tablespoons honey",
+    "1/3 cup soy sauce (such as Silver Swan®)",
+    "1/4 cup Asian (toasted) sesame oil",
+    "3 tablespoons Asian chili garlic sauce",
+    "3 tablespoons minced garlic",
+    "salt to taste",
+    "8 skinless, boneless chicken thighs"
+  ],
+  "cuisine": "Asian",
+  "dishType": "main_course",
+  "image": "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+  "duration": 40,
+  "creator": "Chef LePapu"
+})
+
+  })
+  .then((response) => {
+    console.log(response.title);
+  })
+  .then(() => {
+    return Recipe.insertMany(allRecipes)
+  })
+  .then((response) => {
+
+    response.forEach(element => {
+      console.log(element.title);
+    });
+    
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100})
+  })
+  .then(() => {
+    console.log("Change completed");
+  })
+  .then(() => {
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(() => {
+    console.log("Delete completed");
+  })
+  .then(() => {
+    console.log("Shut Down");
+    return mongoose.connection.close()
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
